e4d472dbcefe8b1dd940c8cb39e0365580350ecc
ssorgatem <ssorgatem@17353060-14a6-11df-853d-23fda6d85a5e>
2010-02-08 12:45:21 +0000

    Ja l'he començat, pero sense utilitzar bioperl...

--- trunk/script10.pl
+++ trunk/script10.pl
@@ -1,1 +1,20 @@
-#!/usr/bin/env perl
+#!/usr/bin/env perl -w
+#Descripció de l'script 10
+# Adrià Cereto Massagué <adrian.cereto@estudiants.urv.cat>, David Carrasco Flores 
+
+use Bio::Seq #Importem mòduls útils
+
+$Seq = ""; #Inicialitza la variable on guardarem la seqüència
+
+$InputFile = "seq_script7"; # Fitxer d'entrada. Ho hem de canviar per a que sigui el primer argument, o enbolcallar-ho en un for per a cada argument.
+
+open INPUT, "<$InputFile";  # Obre el fitxer d'entrada
+  while ($line = <INPUT>){ #Bucle per passar les línies sense \n a la variable $Seq
+    chomp $line;
+    $Seq = $Seq.$line;
+  }
+close INPUT; #Tanquem el fitxer, ja no el necessitem obert
+
+#Ara toca començar-ne el procesament
+
+print "$Seq \n";
