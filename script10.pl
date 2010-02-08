2e209b9e5a13d60746b707d845ed1e1854b74f5a
ssorgatem <ssorgatem@17353060-14a6-11df-853d-23fda6d85a5e>
2010-02-08 15:05:32 +0000

    Bastant avançat

--- script10.pl
+++ script10.pl
@@ -2,19 +2,43 @@
 #Descripció de l'script 10
 # Adrià Cereto Massagué <adrian.cereto@estudiants.urv.cat>, David Carrasco Flores 
 
-use Bio::Seq #Importem mòduls útils
+use Bio::Seq; #Importem mòduls útils
+use Bio::SeqIO;
 
-$Seq = ""; #Inicialitza la variable on guardarem la seqüència
+foreach $NumArgs (0 .. $#ARGV) { #Bucle per a dur a terme les operacions sobre tots els fitxers passats com a arguments
 
-$InputFile = "seq_script7"; # Fitxer d'entrada. Ho hem de canviar per a que sigui el primer argument, o enbolcallar-ho en un for per a cada argument.
+   $RawSeq = ""; #Inicialitza la variable on guardarem la seqüència, per a cada volta del bucle
+   $InputFile = $ARGV[$NumArgs];
 
-open INPUT, "<$InputFile";  # Obre el fitxer d'entrada
-  while ($line = <INPUT>){ #Bucle per passar les línies sense \n a la variable $Seq
-    chomp $line;
-    $Seq = $Seq.$line;
+  open INPUT, "<$InputFile";  # Obre el fitxer d'entrada
+    while ($line = <INPUT>){ #Bucle per passar les línies sense \n a la variable $RawSeq
+      chomp $line;
+      $RawSeq = $RawSeq.$line;
+    }
+  close INPUT; #Tanquem el fitxer, ja no el necessitem obert
+
+  #Ara toca començar-ne el procesament
+
+  print "$RawSeq \n"; #Mostrem el contingut de la variable.
+  
+  $Seq = Bio::PrimarySeq->new( -seq => $RawSeq,  #Creem un objecte Bio::PrimarySeq a partir del contingut de la variable $RawSeq
+			      -id  => 'id_qualsevol', #Qualsevol valor ens servirà per accession_number i id, ja que no els farem servir.
+			      -accession_number => '',
+  );
+
+  #$PrintableSeq = $Seq->seq();
+  $len = $Seq->length(); # Calculem la longitud de la cadena
+
+  if ($len%3 == 0){ #Comprovem si és divisible entre 3, i si no ho és abortem
+    print "és múltiple de 3\n";
+  } else {
+    print "Seqüencia incompleta! No és múltiple de 3\n";
+    break
   }
-close INPUT; #Tanquem el fitxer, ja no el necessitem obert
 
-#Ara toca començar-ne el procesament
+  print $Seq->alphabet;
 
-print "$Seq \n";
+  print "\n";
+  print $@;
+  print  "\n";
+}
