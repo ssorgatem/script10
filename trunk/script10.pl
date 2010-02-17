30924c09ba6d19c84365302365011c42e7e09aa3
dcf1007 <dcf1007@17353060-14a6-11df-853d-23fda6d85a5e>
2010-02-17 11:44:23 +0000

    Millorada l'ajuda

--- trunk/script10.pl
+++ trunk/script10.pl
@@ -1,10 +1,26 @@
 #!/usr/bin/perl -w
+use Getopt::Long; #S'emprara la funció Getopt per recuperar els arguments de l'script
+
 #Script per a traduir una seqüència de DNA a proteines, comprobant que sigui DNA i que tingui els codons necesaris
-# Adrià Cereto Massagué <adrian.cereto@estudiants.urv.cat>, David Carrasco Flores <noemseelteuemail@estudiants.urv.cat>
+# Adrià Cereto Massagué <adrian.cereto@estudiants.urv.cat>, David Carrasco Flores <david.carrascof@estudiants.urv.cat>
 
 #Comencem a definir coses
-$Usage = "Ús:\nperl script10.pl [OPCIONS] ARXIU1 [ARXIU2] [ARXIU3] ...\nO bé:\nperl script10.pl --translate SEQÜÈNCIA1 [SEQÜÈNCIA2] [SEQÜÈNCIA3] ...\n\nPossibles opcions:\n\n--transl_table TRANS_TABLE\n\nEls valors de TRANSL_TABLE es poden trobar a http://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi\n\nSi s'executa sense arguments, demanarà l'entrada manual de la seqüència d'un gen\n"; #Missatge d'ajuda per a l'ús de l'script
+$Usage = "Ús:
+    perl script10.pl [OPCIONS] ARXIU1 [ARXIU2] [ARXIU3] ...
+O bé:
+    perl script10.pl --translate SEQÜÈNCIA1 [SEQÜÈNCIA2] [SEQÜÈNCIA3] ...
+
+Possibles opcions:
+    --transl_table TRANS_TABLE
+      Els valors de TRANSL_TABLE es poden trobar a http://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi
+
+Si s'executa sense arguments, demanarà l'entrada manual de la seqüència d'un gen\n"; #Missatge d'ajuda per a l'ús de l'script
+
 $patro = 0; #Posició des d'on començar a llegir la cadena de DNA. Pot ser 0, 1 o 2. No gaire útil, per ara.
+
+$trans_table; #Declaració de la variable per la taula de traducció del DNA
+
+@translate; #Array per les possibles cadenes de DNA a traduir
 
 #Codi genetic estandard
 %CodiGenetic = ( #Fem un Hash enmagatzemant el codi genetic
@@ -261,6 +277,18 @@ sub DNA2aa { #Subrutina per a traduir la cadena de DNA prèviament validada i pr
   return $Proteina; #Retornem la proteina
 }
 
+sub recupera_ARGV {
+  eval{
+    $SIG{__WARN__}= sub {
+      print $Usage;
+      die;
+    };
+    
+    GetOptions ('translate=s' => \@translate);
+    GetOptions ('trans_table=i' => \$trans_table);
+
+  };
+}
 #Aquí comença l'execució
 
 if($ARGV[0] eq "--transl_table"){ #Per si volem fer servir un altre codi genètic
